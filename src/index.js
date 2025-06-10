10d9174ba96646b5f92cedc018538db0365e125c
md.moinuddin <md.moinuddin@nzminds.com>
2025-06-10 13:12:14 +0530

    afs

--- src/index.js
+++ src/index.js
@@ -16,11 +16,9 @@ const cognitoAuthConfig = {
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
+    <AuthProvider {...cognitoAuthConfig} storage="localStorage">
       <App />
     </AuthProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
